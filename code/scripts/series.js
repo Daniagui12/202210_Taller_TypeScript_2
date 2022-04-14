13af6905828e44fe4cef9d647d38ad2e2fa90bd6
Daniel Aguilera <d.aguilera@uniandes.edu.co>
2022-04-13 21:09:27 -0500

    Actualizacion

---
+++ code/scripts/series.js
@@ -0,0 +1,13 @@
+var Serie = /** @class */ (function () {
+    function Serie(id, name, channel, seasons, description, watchUrl, imgUrl) {
+        this.id = id;
+        this.name = name;
+        this.channel = channel;
+        this.seasons = seasons;
+        this.imgUrl = imgUrl;
+        this.description = description;
+        this.watchUrl = watchUrl;
+    }
+    return Serie;
+}());
+export { Serie };
